e03d057521a9c201e9e79b3aaa36b7a24b0a98fb
Onur YILDIZ <52601509+onyor@users.noreply.github.com>
2021-02-14 20:39:42 +0300

    Add project files.

---
+++ CSharpCamp/DictionaryResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpCamp
+{
+    class DictionaryResult
+    {
+        public Boolean Status { get; set; }
+        public string Message { get; set; }
+        public Object Data { get; set; }
+    }
+}
